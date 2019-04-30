f8542291cbdb97d69305dfc962ab3071ec1d5367
[Desdinova] <desdinovy@gmail.com>
2019-04-30 17:08:09 +0200

    added testinput for influx

--- avucheck.py
+++ avucheck.py
@@ -1,1 +1,6 @@
- 
+#!/usr/bin/env python3
+
+import requests
+
+avuXml = requests.get("http://212.89.102.251:8180/monitor/")
+print(avuXml.text) 
---
+++ testinput.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import time
+
+print("weather,location=us-midwest,season=summer temperature=82 1465839830100400200")
+
